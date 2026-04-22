f364dcd62880304e05766b74d3bd0c23d852db5c
UmbreonRyguy <73410428+UmbreonRyguy@users.noreply.github.com>
2026-04-22 14:07:19 -0700

    fixed issue with file paths

--- script.js
+++ script.js
@@ -17,25 +17,25 @@ class Cinematic extends Phaser.Scene {
         super("Cinematic");
     }
     preload() {
-        this.load.image("starsbg", '/assets/starsBackground.jpg')
-        this.load.image("WispFull", '/assets/WispMoving.gif')
+        this.load.image("starsbg", 'assets/starsBackground.jpg')
+        this.load.image("WispFull", 'assets/WispMoving.gif')
         
-        this.load.audio("Wisp1", '/assets/wisp1.wav')
-        this.load.image("WispAlone", '/assets/WispAlone.gif')
+        this.load.audio("Wisp1", 'assets/wisp1.wav')
+        this.load.image("WispAlone", 'assets/WispAlone.gif')
 
-        this.load.image("line1", '/assets/line1.png')
-        this.load.image("line2", '/assets/line2.png')
-        this.load.image("playText", '/assets/playText.png')
-        this.load.image("loadText", '/assets/loadText.png')
-        this.load.image("settingsText", '/assets/settingsText.png')
+        this.load.image("line1", 'assets/line1.png')
+        this.load.image("line2", 'assets/line2.png')
+        this.load.image("playText", 'assets/playText.png')
+        this.load.image("loadText", 'assets/loadText.png')
+        this.load.image("settingsText", 'assets/settingsText.png')
 
-        this.load.image("loadingText", '/assets/loadingText.png')
+        this.load.image("loadingText", 'assets/loadingText.png')
 
-        this.load.image("wispTitle", '/assets/wispTitle.png')
-        this.load.image("star", '/assets/star.png')
+        this.load.image("wispTitle", 'assets/wispTitle.png')
+        this.load.image("star", 'assets/star.png')
 
-        this.load.audio("Wisp2", '/assets/wisp2.wav')
-        this.load.audio("bgm", '/assets/bg.mp3')
+        this.load.audio("Wisp2", 'assets/wisp2.wav')
+        this.load.audio("bgm", 'assets/bg.mp3')
 
     }
     create() {
